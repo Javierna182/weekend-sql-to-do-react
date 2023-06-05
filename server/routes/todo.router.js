b13acf7ffa1181d0adafce7968da92ad80f5c7f4
Javier Navarro <javierna182@gmail.com>
2023-06-04 21:42:29 -0500

    setted up GET

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -4,7 +4,17 @@ const pool = require('../modules/pool.js');
 
 // GET -- Setup a GET route to get all the items from the database
 router.get('/', (req, res) => {
-    const sqlText = `SELECT * FROM ""`
+    const sqlText = `SELECT * FROM "tasks" ORDERED BY task;`;
+    pool.query(sqlText)
+    .then((result) => {
+        console.log(`Got stuff back from the database`, result);
+        res.send(result.rows)
+    })
+    .catch((error) => {
+        console.log(`Error making database query ${sqlText}`, error);
+        res.sendStatus(500); 
+    })    
+
 })
 
 // POST
