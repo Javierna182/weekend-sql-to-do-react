1f5a47d04eccdd144e9ec2446d8143ab95e516f5
Javier Navarro <javierna182@gmail.com>
2023-06-05 21:39:15 -0500

    created function addTask

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,31 @@
+import React from 'react';
 import {useState} from 'react';
+// import { useEffect, useState } from 'react';
+import './App.css';
+
+function getTask(){
+  return fetch('/todo.router')
+  .then(response => response.json())
+  .catch((error) => {
+    console.log(error);
+  });
+}
+
+function addTask(task){
+  return fetch('/todo.router', {
+    method: 'POST',
+    body: JSON.stringify(task),
+    headers: { 'Content-Type': 'application/json' }
+  })
+  .then((response) => {
+    if (response.status !== 201) {
+      throw new Error('Bad status!');
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+}
 
 function App () {
   
